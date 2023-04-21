1a9d1b6e45b33d123052f7ff84f450aaf4b42600
huixiannnxd <47735062+huixiannnxd@users.noreply.github.com>
2023-04-22 00:05:23 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,48 @@
 // Initialise
-document.addEventListener("DOMContentLoaded", setUp);
+// document.addEventListener("DOMContentLoaded", setUp);
+
+// Create starting elements
+var num_array = [1, 2, 3, 4];
+var option = "wind";
 
 // Function for initialisation
 var cardsBack = document.querySelectorAll(".card-back");
 function setUp() {
   // Randomising winds
-  var num_array = [1, 2, 3, 4];
-  shuffle(num_array);
-  console.log(num_array);
-  // Assigning pics according to randomisation
-  var counter = 0;
-  while (counter < cardsBack.length) {
-    cardsBack[counter].classList.add("img" + num_array[counter]);
-    counter++;
-  }
+  resetCardBack();
+  resetCard();
+  assgPics();
 }
 
-// Flipping of cards
+// Allow for flipping of cards
 var cards = document.querySelectorAll(".single-card");
 
 for (var i = 0; i < cards.length; i++) {
   cards[i].addEventListener("click", onClick);
 }
 
+// Assign pics according to randomisation
+function assgPics() {
+  shuffle(num_array);
+  var counter = 0;
+  while (counter < cardsBack.length) {
+    cardsBack[counter].classList.add(option + num_array[counter]);
+    counter++;
+  }
+}
+
+// Button changer
+var windButton = document.querySelector(".wind");
+var animalButton = document.querySelector(".animal");
+windButton.addEventListener("click", changeWind);
+animalButton.addEventListener("click", changeAnimal);
+
+//Function to flip cards
 function onClick() {
   this.classList.toggle("flip-card");
 }
 
+// Function to shuffle num_array
 function shuffle(input_array) {
   for (var i = input_array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
@@ -35,3 +51,46 @@ function shuffle(input_array) {
     input_array[j] = temp;
   }
 }
+
+function changeWind() {
+  option = "wind";
+  setUp();
+}
+
+function changeAnimal() {
+  option = "animal";
+  setUp();
+}
+
+function resetCard() {
+  for (var i = 0; i < cards.length; i++) {
+    if (cards[i].classList.contains("flip-card")) {
+      cards[i].classList.remove("flip-card");
+    } else {
+      continue;
+    }
+  }
+}
+
+var imgClasses = [
+  "wind1",
+  "wind2",
+  "wind3",
+  "wind4",
+  "animal1",
+  "animal2",
+  "animal3",
+  "animal4",
+];
+function resetCardBack() {
+  for (var i = 0; i < cardsBack.length; i++) {
+    for (var j = 0; j < imgClasses.length; j++) {
+      if (cardsBack[i].classList.contains(imgClasses[j])) {
+        cardsBack[i].classList.remove(imgClasses[j]);
+      } else {
+        continue;
+      }
+    }
+  }
+}
+
